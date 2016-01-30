886ddfacd3d7a49c9fa48ec93979ecf6b8fded61
Nobody <YouDontHave@MyEmail.com>
2016-01-30 00:17:42 -0200

    Primeira refatoração do Gerador de CC.

--- cartao-credito.py
+++ cartao-credito.py
@@ -1,58 +1,49 @@
 # -*- coding: UTF-8 -*-
-import time, random
-start = time.time()
-def cc_issuer():
-	global first_numbers
-	first_numbers = []
-	global issuing_network
-	issuing_network = ''
-	print 'Digite 1 para obter um cartão de crédito de bandeira Visa, 2 para MasterCard.'
-	choice = raw_input()
-	if(choice == '1'):
-		first_numbers.append(4)
-		issuing_network = 'Visa'
-	elif(choice == '2'):
-		issuer_one = 5
-		issuer_two = random.randint(1,5)
-		issuer = (issuer_one, issuer_two)
-		first_numbers.extend(issuer)
-		issuing_network = 'MasterCard'
-	while(len(first_numbers) < 4):
-		number = random.randint(0,9)
-		first_numbers.append(number)
-	print first_numbers
+import random, time 
+def cc_issuer(choice):
+	issuer_choice = {'1':'Visa', '2':'MasterCard'}
+	issuer_choice_number = {'1':'4', '2':str(random.randint(1,5))}
+	return issuer_choice[choice], issuer_choice_number[choice]
 def random_eleven():
-	global random_numbers
-	random_numbers = []
-	random_numbers += first_numbers
-	while(len(random_numbers) < 15):
-		number = random.randint(0,9)
-		random_numbers.append(number)
-	print random_numbers
-def verified_numbers():
-	new_numbers = []
-	counter = 1
-	for number in random_numbers:
+	random_numbers = ''
+	for number in range(14):
+		rand_number = random.randint(0,9)
+		random_numbers += str(rand_number)
+	return random_numbers
+def verified_numbers(cc_number):
+	new_numbers = 0
+	counter = 0
+	for number in cc_number:
 		if(counter % 2 == 0):
-			new_numbers.append(number)
-		else:
-			odd_result = number * 2
+			odd_result = int(number) * 2
 			if(odd_result > 9):
 				odd_result -= 9
-			new_numbers.append(odd_result)
+			new_numbers += odd_result
+		else:
+			new_numbers += int(number)
 		counter += 1
-	sum_new_numbers = sum(new_numbers)
-	print new_numbers
-	print sum_new_numbers
-	if((sum_new_numbers < 150) and (sum_new_numbers % 10 == 0)):
-		random_numbers.append(0)
+	if((new_numbers < 150) and (new_numbers % 10 == 0)):
+		verifier_number = '0'
 	else:
-		rest = sum_new_numbers % 10
+		rest = new_numbers % 10
 		last_number = 10 - rest
-		random_numbers.append(last_number)
-cc_issuer()
-random_eleven()
-verified_numbers()
-elapsed = (time.time() - start)
-print 'Seu cartão de crédito de bandeira %s foi gerado com o número %s em %s segundos' %(issuing_network, random_numbers, elapsed)
-#UMA REFATORAÇÃO E UM APRIMORAMENTO DEIXARIA AS COISAS MAIS BACANAS :^)
+		verifier_number = str(last_number)
+	return verifier_number
+def cc():
+	option = ''
+	while(option != '0'):
+		print 'Digite 1 para obter um cartão de crédito de bandeira Visa, 2 para MasterCard. Digite 0 para encerrar o programa.'
+		option = raw_input()
+		start = time.time()
+		if(option != '0'):
+			print '\n\n\n\n\n'*2
+			issuer, cc_number = cc_issuer(option)
+			random_nums = random_eleven()
+			cc_number += random_nums
+			vf_n = verified_numbers(cc_number)
+			cc_number += vf_n
+			print '\aNúmero do Cartão de Crédito: %s\n\n' %(cc_number)
+			print 'Bandeira: %s\n\n' %(issuer)
+			elapsed = (time.time() - start)
+			print 'Seu Cartão de Crédito foi gerado em %s segundos.\n\n\n\n\n' %(elapsed)
+cc()
